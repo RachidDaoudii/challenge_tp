994dc5995d0b96c246ded6eeb258891e10f39776
RachidDaoudii <rachiddaoudi553@gmail.com>
2022-09-07 17:31:14 +0100

    Modifer challenge13

--- Challenge13/main.c
+++ Challenge13/main.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+int main(){
 
-int main()
-{
-    int nombre;
-    printf("donner un nombre :");
-    scanf("%d",&nombre);
-    return 0;
+  int tab[10], nbr, i;
+  char _char;
+
+  printf("donner un caracterer :");
+  scanf("%s",&_char);
+
+
+  printf("Entrez le nombre à convertir: ");
+  scanf("%d",&nbr);
+
+  for(i=0; nbr > 0; i++)
+  {
+    tab[i] = nbr%2;
+    nbr = nbr/2;
+  }
+
+  printf("\nLe nombre binaire est = ");
+
+  for(i=i-1; i >= 0; i--)
+  {
+    printf("%d",tab[i]);
+  }
+    printf("le caracterer %s en hexadecimal est :%x \n",&_char,_char);
+  return 0;
 }
