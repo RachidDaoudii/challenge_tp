61aca147b127212a972f90a851ec4ac3e27c812e
RachidDaoudii <112935514+RachidDaoudii@users.noreply.github.com>
2022-09-07 23:52:48 +0200

    Update main.c

--- Challenge13/main.c
+++ Challenge13/main.c
@@ -2,28 +2,10 @@
 #include <stdlib.h>
 int main(){
 
-  int tab[10], nbr, i;
-  char _char;
-
-  printf("donner un caracterer :");
-  scanf("%s",&_char);
-
-
-  printf("Entrez le nombre à convertir: ");
-  scanf("%d",&nbr);
-
-  for(i=0; nbr > 0; i++)
-  {
-    tab[i] = nbr%2;
-    nbr = nbr/2;
-  }
-
-  printf("\nLe nombre binaire est = ");
-
-  for(i=i-1; i >= 0; i--)
-  {
-    printf("%d",tab[i]);
-  }
-    printf("le caracterer %s en hexadecimal est :%x \n",&_char,_char);
+    int nb ;
+    printf("Donner un nombre :");
+    scanf("%d",&nb);
+    printf("nombre en hexadecimal est : %X \n",nb);
+    printf("nombre en octal est : %o \n",nb);
   return 0;
 }
